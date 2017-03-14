ed46f418bcb4e516e0fec07da92cf27439b1b7dc
ustbhuangyi <280309453@qq.com>
2017-03-14 17:34:21 +0800

    ua

--- src/util/dom.js
+++ src/util/dom.js
@@ -2,6 +2,6 @@
 
 export const inBrowser = typeof window !== 'undefined'
 
-export const isIOS901 = inBrowser && /iPhone OS 9_[01]]/i.test(window.userAgent)
+export const isIOS901 = inBrowser && /iPhone OS 9_[01]]/i.test(window.navigator.userAgent)
 
 export const isWechatWK = inBrowser && window.__wxjs_is_wkwebview
